cb33c32a89e091187609edee1583e92438a83cbe
Leung Wong <wguoliang1113@gmail.com>
2016-09-25 22:12:09 +0200

    Adds script to clean up build folder

--- make.cmd
+++ make.cmd
@@ -1,3 +1,12 @@
-set NODE_ENV=%1
-echo %NODE_ENV%
-webpack
+if %1%==clean (
+    rmdir /S /Q dist
+    exit
+)
+
+if %1%==production set task=T
+if %1%==development set task=T
+
+if %task%==T (
+    set NODE_ENV=%1
+    webpack
+)
